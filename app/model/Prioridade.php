36458030b149e9a9a5d552bca770b4bf2cddffbe
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2024-05-12 18:43:26 -0300

    Cricação dos atributos privados da classe Prioridade e seus respectivos métodos acessores

--- app/model/Prioridade.php
+++ app/model/Prioridade.php
@@ -1,5 +1,37 @@
 <?php
     class Prioridade{
+        //Atributos
+        private string $descricao;
+        private int $peso;
 
+        //Métodos acessores
+
+        /**
+         * Atribuí o valor informado para o atributo privado $descricao
+         */
+        public function setDescricao(string $descricao):void{
+            $this->descricao = $descricao;
+        }
+
+        /**
+         * Retorna o valor do atributo privado $descricao
+         */
+        public function getDescricao():string{
+            return $this->descricao;
+        }
+
+        /**
+         * Atribuí o valor informado para o atributo privado $peso
+         */
+        public function setPeso(int $peso):void{
+            $this->peso = $peso;
+        }
+
+        /**
+         * Retorna o valor do atributo privado $peso
+         */
+        public function getPeso():int{
+            return $this->peso;
+        }
     }
 ?>
